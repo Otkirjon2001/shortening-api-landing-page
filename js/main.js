117e7f31b486fe3c2d8be9ef3d2e4cfa8baa4210
Otkirjon2001 <otkirsalimboyev@gmail.com>
2022-04-22 11:12:17 +0300

    Add shorten section other styles

--- js/main.js
+++ js/main.js
@@ -1,7 +1,21 @@
 const elSiteForm = document.querySelector('.js-shorten-form');
 const elResultsList = document.querySelector('.js-results-list');
-elSiteForm.addEventListener('submit', function(evt){
-  evt.preventDefault();
-
-  elResultsList.classList.add('results--open');
-});
+if(elSiteForm){
+  elSiteForm.addEventListener('submit', function(evt){
+    evt.preventDefault();
+    elResultsList.classList.add('results--open');
+  });
+}
+if(elResultsList){
+  elResultsList.addEventListener('click', function(evt){
+    if(evt.target.matches('.js-results-button')){
+      evt.target.textContent = 'Copied!';
+      evt.target.classList.add('button--open');
+      navigator.clipboard.writeText(evt.target.previousElementSibling.textContent);
+    }
+    setTimeout(function(){
+      evt.target.classList.remove('button--open');
+      evt.target.textContent = 'Copy';
+    },1000)
+  })
+}
